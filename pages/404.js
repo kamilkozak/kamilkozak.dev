b4ff547220f5cc72f80fd97270063c53e235eff3
kamilkozak <kamilaavill@gmail.com>
2023-06-06 19:25:13 +0200

    404

--- pages/404.js
+++ pages/404.js
@@ -1,9 +1,13 @@
 import Link from '@/components/Link'
 import { PageSEO } from '@/components/SEO'
+import { Button } from '@/components/Button'
 export default function FourZeroFour() {
   return (
     <>
-      <PageSEO title="Page Not Found" description="Sorry we couldn't find this page :(" />
+      <PageSEO
+        title="Nie znaleziono strony"
+        description="Strona o ponadnym adresie nie została odnaleziona."
+      />
       <div className="flex flex-col items-start justify-start md:mt-24 md:flex-row md:items-center md:justify-center md:space-x-6">
         <div className="space-x-2 pb-8 pt-6 md:space-y-5">
           <h1 className="text-6xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 md:border-r-2 md:px-6 md:text-8xl md:leading-14">
@@ -11,18 +15,9 @@ export default function FourZeroFour() {
           </h1>
         </div>
         <div className="max-w-md">
-          <p className="mb-4 text-xl font-bold leading-normal md:text-2xl">
-            Sorry we couldn't find this page.
-          </p>
-          <p className="mb-8">
-            But dont worry, you can find plenty of other things on our homepage.
-          </p>
-          <Link
-            href="/"
-            className="focus:shadow-outline-blue inline rounded-lg border border-transparent bg-blue-600 px-4 py-2 text-sm font-medium leading-5 text-white shadow transition-colors duration-150 hover:bg-blue-700 focus:outline-none dark:hover:bg-blue-500"
-          >
-            Back to homepage
-          </Link>
+          <p className="mb-4 text-xl font-bold leading-normal md:text-2xl">Ups, nic tu nie ma</p>
+          <p className="mb-6">Ta strona nie istnieje, ale są inne...</p>
+          <Button href="/">Powrót do strony głównej</Button>
         </div>
       </div>
     </>
