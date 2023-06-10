8798ecc17ae73059d7353e878a80301de9e95c4e
kamilkozak <kamilaavill@gmail.com>
2023-06-10 11:59:41 +0200

    remove projects

--- pages/projects.js
+++ pages/projects.js
@@ -2,18 +2,17 @@ import siteMetadata from '@/data/siteMetadata'
 import projectsData from '@/data/projectsData'
 import Card from '@/components/Card'
 import { PageSEO } from '@/components/SEO'
+import { Container } from '@/components/Container'
 export default function Projects() {
   return (
-    <>
+    <Container>
       <PageSEO title={`Projects - ${siteMetadata.author}`} description={siteMetadata.description} />
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
         <div className="space-y-2 pb-8 pt-6 md:space-y-5">
           <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
             Projects
           </h1>
-          <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
-            Showcase your projects with a hero image (16 x 9)
-          </p>
+          <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">...</p>
         </div>
         <div className="container py-12">
           <div className="-m-4 flex flex-wrap">
@@ -29,6 +28,6 @@ export default function Projects() {
           </div>
         </div>
       </div>
-    </>
+    </Container>
   )
 }
